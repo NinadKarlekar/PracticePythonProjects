fa0d7f87613bd6bab22bbcedc3fd3f8c313921ef
NinadKarlekar <ninad.karlekar@vsit.edu.in>
2021-09-12 22:50:50 +0530

    Create QR generator.py

---
+++ QR generator.py
@@ -0,0 +1,13 @@
+import pyqrcode 
+from pyqrcode import QRCode 
+
+# String which represent the QR code 
+s = "https://youtu.be/zkqAhBidoEI"
+
+# Generate QR code 
+url = pyqrcode.create(s) 
+
+# Create and save the png file naming "myqr.png" 
+url.svg("myyoutube.svg", scale = 8)
+
+
